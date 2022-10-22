1d4eb3c58ee766a7487ba1b2fe927035f4549769
gabogh123 <gabogh99@gmail.com>
2022-10-22 00:36:59 -0600

    feat(Team_In_Tournament): Add model

---
+++ WorldCupOnline-API/WorldCupOnline-API/Models/Team_In_Tournament.cs
@@ -0,0 +1,8 @@
+﻿namespace WorldCupOnline_API.Models
+{
+    public class Team_In_Tournament
+    {
+        public string teamid { get; set; }
+        public string tournamentid { get; set; }
+    }
+}
