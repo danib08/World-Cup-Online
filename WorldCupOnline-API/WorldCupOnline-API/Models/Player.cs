5df08f9f3c6b5a5406eb5b8be7bf914679274aa1
gabogh123 <gabogh99@gmail.com>
2022-10-22 00:36:15 -0600

    feat(Player): Add model

---
+++ WorldCupOnline-API/WorldCupOnline-API/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace WorldCupOnline_API.Models
+{
+    public class Player
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public string position { get; set; }
+        
+    }
+}
