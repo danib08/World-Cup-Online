655d515dee8eccdcaed13e2735cba151cfd92b69
gabogh123 <gabogh99@gmail.com>
2022-10-22 00:37:14 -0600

    feat(Player_In_Team): Add model

---
+++ WorldCupOnline-API/WorldCupOnline-API/Models/Player_In_Team.cs
@@ -0,0 +1,9 @@
+﻿namespace WorldCupOnline_API.Models
+{
+    public class Player_In_Team
+    {
+        public string teamid { get; set; }
+        public string playerid { get; set; }
+        public int jerseynum { get; set; }
+    }
+}
