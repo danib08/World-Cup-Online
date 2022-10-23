48ad72d3df22ce222e53d097e482b2e8faf7c08c
gabogh123 <gabogh99@gmail.com>
2022-10-22 22:11:12 -0600

    feat(Models): Add missing models

---
+++ WorldCupOnline-API/WorldCupOnline-API/Models/Match.cs
@@ -0,0 +1,13 @@
+﻿namespace WorldCupOnline_API.Models
+{
+    public class Match
+    {
+        public int id { get; set; }
+        public DateTime startdate { get; set; }
+        public DateTime starttime { get; set; }
+        public string score { get; set; }
+        public string location { get; set; }
+        public string state { get; set; }
+        public string tournamentid { get; set; }
+    }
+}
