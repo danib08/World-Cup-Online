e9c927ed101b0238f7fffe83681fcb9bdfa89a1e
gabogh123 <gabogh99@gmail.com>
2022-10-23 18:00:50 -0600

    doc(Controllers): Add comments using XML

--- WorldCupOnline-API/WorldCupOnline-API/Controllers/StateController.cs
+++ WorldCupOnline-API/WorldCupOnline-API/Controllers/StateController.cs
@@ -14,165 +14,164 @@ namespace WorldCupOnline_API.Controllers
     {
         private readonly IConfiguration _configuration;
 
+        /// <summary>
+        /// Established configuration for controller to get connection
+        /// </summary>
+        /// <param name="configuration"></param>
         public StateController(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
+        /// <summary>
+        /// Method to get all created states
+        /// </summary>
+        /// <returns>JSONResult with all states</returns>
         [HttpGet]
         public JsonResult GetStates()
         {
-            string query = @"stored procedure";
+            string query = @"exec proc_state '','Select'"; ///sql query
 
-            DataTable table = new DataTable();
+            DataTable table = new DataTable(); //Create datatable
             string sqlDataSource = _configuration.GetConnectionString("WorldCupOnline");
             SqlDataReader myReader;
             using (SqlConnection myCon = new SqlConnection(sqlDataSource))
             {
-                myCon.Open();
+                myCon.Open(); ///Open connection
                 using (SqlCommand myCommand = new SqlCommand(query, myCon))
                 {
                     myReader = myCommand.ExecuteReader();
-                    table.Load(myReader);
+                    table.Load(myReader); ///Data is loaded into table
                     myReader.Close();
-                    myCon.Close();
+                    myCon.Close(); ///Closed connection
                 }
             }
 
             TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
             foreach (DataColumn column in table.Columns)
             {
-                column.ColumnName = ti.ToLower(column.ColumnName);
+                column.ColumnName = ti.ToLower(column.ColumnName); ///Make all lowercase to avoid conflicts with communication
             }
 
-            return new JsonResult(table);
+            return new JsonResult(table); ///Return JSON Of the data table
         }
 
+        /// <summary>
+        /// Method to get one state by its name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         [HttpGet("{name}")]
         public string GetState(string name)
         {
-
+            ///Created label
             string lbl_name;
 
 
-            //SQL Query
+            ///SQL Query
             string query = @"
-                            stored procedure";
-            DataTable table = new DataTable();//Created table to store data
+                            exec proc_state @name,'Select One'";
+
+            DataTable table = new DataTable();///Created table to store data
             string sqlDataSource = _configuration.GetConnectionString("WorldCupOnline");
             SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDataSource))//Connection created
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))///Connection created
             {
-                myCon.Open();//Open connection
-                using (SqlCommand myCommand = new SqlCommand(query, myCon))//Command with query and connection
+                myCon.Open();///Open connection
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))///Command with query and connection
                 {
-                    //Added parameters
+                    ///Added parameters
                     myCommand.Parameters.AddWithValue("@name", name);
                     myReader = myCommand.ExecuteReader();
-                    table.Load(myReader); //Load data to table
+                    table.Load(myReader); ///Load data to table
                     myReader.Close();
-                    myCon.Close(); //Close connection
+                    myCon.Close(); ///Close connection
                 }
             }
 
+            ///Verify if table is empty
             if (table.Rows.Count > 0)
             {
 
                 DataRow row = table.Rows[0];
 
+                ///Manipulation of every row of datatable and parse them to string
                 lbl_name = row["name"].ToString();
 
+                ///Creation of the JSON
                 var data = new JObject(new JProperty("name", lbl_name));
 
-                return data.ToString();
+                return data.ToString(); ///Return created JSON
             }
             else
             {
                 var data = new JObject(new JProperty("Existe", "no"));
-                return data.ToString();
+                return data.ToString(); ///Return message if table is empty
             }
 
         }
 
-
+        /// <summary>
+        /// Method to create states
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns>JSON of the state created</returns>
         [HttpPost]
         public JsonResult PostState(State state)
         {
             //SQL Query
             string query = @"
-                             stored procedure
+                             exec proc_state @name,'Insert'
                             ";
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("WorldCupOnline");
             SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDataSource))//Connection stablished
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))///Connection stablished
             {
-                myCon.Open(); //Opened connection
+                myCon.Open(); ///Opened connection
                 SqlCommand myCommand = new SqlCommand(query, myCon);
 
-                //Parameters added with values
+                ///Parameters added with values
                 myCommand.Parameters.AddWithValue("@name", state.name);
                 myReader = myCommand.ExecuteReader();
                 table.Load(myReader);
                 myReader.Close();
-                myCon.Close();//Closed connection
+                myCon.Close();///Closed connection
 
             }
 
-            return new JsonResult(table); //Returns table with info
+            return new JsonResult(table); ///Returns table with info
 
         }
 
-
-        [HttpPut]
-        public ActionResult PutState(State state)
-        {
-            //SQL Query
-            string query = @"
-                             stored procedures
-                            ";
-            DataTable table = new DataTable();
-            string sqlDataSource = _configuration.GetConnectionString("WorldCupOnline");
-            SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDataSource))//Connection started
-            {
-                myCon.Open(); //Connection closed
-                using (SqlCommand myCommand = new SqlCommand(query, myCon))//Sql command with query and connection
-                {
-                    //Added parameters
-                    myCommand.Parameters.AddWithValue("@name", state.name);
-                    myReader = myCommand.ExecuteReader();
-                    table.Load(myReader);
-                    myReader.Close();
-                    myCon.Close();//Closed connection
-                }
-            }
-            return Ok(); //Returns acceptance
-        }
-
+        /// <summary>
+        /// Method to delete a state by its name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         [HttpDelete("{name}")]
         public ActionResult DeleteState(string name)
         {
-            //SQL Query
+            ///SQL Query
             string query = @"
-                            stored procedure
+                            exec proc_state @name,'Delete'
             ";
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("WorldCupOnline");
             SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDataSource))//Connection created
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))///Connection created
             {
-                myCon.Open();//Open connection
-                using (SqlCommand myCommand = new SqlCommand(query, myCon)) //Command with query and connection
+                myCon.Open();///Open connection
+                using (SqlCommand myCommand = new SqlCommand(query, myCon)) ///Command with query and connection
                 {
                     myCommand.Parameters.AddWithValue("@name", name);
                     myReader = myCommand.ExecuteReader();
                     table.Load(myReader);
                     myReader.Close();
-                    myCon.Close();//Closed connection
+                    myCon.Close();///Closed connection
                 }
             }
-            return Ok(); //Returns acceptance
+            return Ok(); ///Returns acceptance
         }
     }
 }
