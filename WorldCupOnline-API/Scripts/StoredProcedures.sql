fe7bbcc42665b84738ab72808b2a5ba3f01bdbbc
Daniela Brenes <danibreo6@gmail.com>
2022-11-12 05:17:20 -0600

    feat(Database ): create country stored procedure

--- WorldCupOnline-API/Scripts/StoredProcedures.sql
+++ WorldCupOnline-API/Scripts/StoredProcedures.sql
@@ -571,3 +571,45 @@ as begin
 	end
 end
 go
+
+create procedure proc_country(@ID varchar(3),
+				@Name varchar(31),
+			    @StatementType varchar(50) = '')
+as begin
+
+	if @StatementType = 'Insert'
+	begin
+		insert into dbo.Country(ID, Name)
+		values(@ID, @Name)
+	end
+
+	if @StatementType = 'Select'
+	begin
+		select * from dbo.Country
+	end
+
+	if @StatementType = 'Select WebApp'
+	begin
+		select ID as label, Name as value
+		from dbo.Country
+	end
+
+	if @StatementType = 'Select One'
+	begin
+		select * from dbo.Country
+		where ID = @ID
+	end
+
+	if @StatementType = 'Update'
+	begin
+		update dbo.Country set Name=@Name
+		where ID=@ID 	
+	end
+
+	if @StatementType = 'Delete'
+	begin
+		delete from dbo.Country
+		where ID = @ID
+	end
+end
+go
